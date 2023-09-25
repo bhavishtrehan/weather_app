633c201a34638df7f4b59e0dfa0278e52ea25809
bhavishtrehan <bhavish48@gmail.com>
2023-09-25 22:48:29 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:ionicons/ionicons.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_icons/weather_icons.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +13,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DateTime currentDate = DateTime.now();
+    String formattedDate = DateFormat.yMMMMd().format(currentDate);
+
     return MaterialApp(
       home: Scaffold(
           backgroundColor: Color(0xFF010C1E),
@@ -28,12 +34,14 @@ class MyApp extends StatelessWidget {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text(
-                        'September 15, 2023',
-                        style: TextStyle(
-                            fontFamily: 'SourceSans3',
-                            color: Colors.white38,
-                            fontSize: 20),
+                      Center(
+                        child: Text(
+                          '$formattedDate',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontFamily: 'SourceSans3'),
+                        ),
                       ),
                       FutureBuilder<Map<String, dynamic>>(
                         future: fetchWeatherData(),
@@ -48,19 +56,23 @@ class MyApp extends StatelessWidget {
                             return Text(
                               "$place",
                               style: TextStyle(
-                                  fontSize: 30,
+                                  fontSize: 60,
                                   fontFamily: 'SourceSans3',
                                   color: Colors.white),
                             );
                           }
                         },
                       ),
-                      Image(
-                        image: AssetImage('images/sunny.png'),
-                        height: 300,
-                        width: 300,
+                      Expanded(
+                        flex: 10,
+                        child: Image(
+                          image: AssetImage('images/sunnyy.png'),
+                          height: 500,
+                          width: 300,
+                        ),
                       ),
                       Expanded(
+                        flex: 8,
                         child: FutureBuilder<Map<String, dynamic>>(
                           future: fetchWeatherData(),
                           builder: (context, snapshot) {
@@ -70,63 +82,160 @@ class MyApp extends StatelessWidget {
                             } else if (snapshot.hasError) {
                               return Text("Error: ${snapshot.error}");
                             } else {
-                              final temperature =
+                              double temperature =
                                   snapshot.data?['current']['temp_c'];
+                              int temp = temperature.toInt();
                               return Text(
-                                "Temperature: $temperature°C",
+                                " $temp°",
                                 style: TextStyle(
-                                    fontSize: 30,
+                                    fontSize: 110,
                                     color: Colors.white,
-                                    fontFamily: 'SourceSans3'),
+                                    // fontWeight: FontWeight.w600,
+                                    fontFamily: 'Work Sans'),
                               );
                             }
                           },
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.all(10),
+                        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
                         height: 140,
                         width: double.infinity,
-                        decoration: const BoxDecoration(
+                        decoration: BoxDecoration(
                             color: Color(0xFF0E55B7),
                             borderRadius:
                                 BorderRadius.all(Radius.circular(60))),
-                        child: const Row(
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Text(
-                                  'Wind',
-                                  style: TextStyle(
-                                      fontFamily: 'SourceSans3',
-                                      color: Colors.white,
-                                      fontSize: 20),
-                                )
-                              ],
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
+                                FutureBuilder<Map<String, dynamic>>(
+                                  future: fetchWeatherData(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return CircularProgressIndicator();
+                                    } else if (snapshot.hasError) {
+                                      return Text("Error: ${snapshot.error}");
+                                    } else {
+                                      final wind =
+                                          snapshot.data?['current']['humidity'];
+                                      return Text(
+                                        "  $wind%",
+                                        style: TextStyle(
+                                            fontFamily: 'SourceSans3',
+                                            color: Colors.white,
+                                            fontSize: 15),
+                                      );
+                                    }
+                                  },
+                                ),
+                                Icon(
+                                  Ionicons.water_outline,
+                                  color: Colors.white,
+                                ),
                                 Text(
                                   'Humidity',
                                   style: TextStyle(
                                       fontFamily: 'SourceSans3',
                                       color: Colors.white,
-                                      fontSize: 20),
+                                      fontSize: 15),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              '|',
+                              style: TextStyle(
+                                  fontSize: 100,
+                                  color: Colors.white24,
+                                  fontWeight: FontWeight.w100),
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FutureBuilder<Map<String, dynamic>>(
+                                  future: fetchWeatherData(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return CircularProgressIndicator();
+                                    } else if (snapshot.hasError) {
+                                      return Text("Error: ${snapshot.error}");
+                                    } else {
+                                      final wind =
+                                          snapshot.data?['current']['wind_kph'];
+                                      return Text(
+                                        "$wind km/h",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontFamily: 'SourceSans3',
+                                            color: Colors.white),
+                                      );
+                                    }
+                                  },
+                                ),
+                                Icon(
+                                  WeatherIcons.windy,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Wind',
+                                  style: TextStyle(
+                                      fontFamily: 'SourceSans3',
+                                      color: Colors.white,
+                                      fontSize: 15),
                                 )
                               ],
                             ),
+                            Text(
+                              '|',
+                              style: TextStyle(
+                                  fontSize: 100,
+                                  color: Colors.white24,
+                                  fontWeight: FontWeight.w100),
+                            ),
                             Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
+                                FutureBuilder<Map<String, dynamic>>(
+                                  future: fetchWeatherData(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return CircularProgressIndicator();
+                                    } else if (snapshot.hasError) {
+                                      return Text("Error: ${snapshot.error}");
+                                    } else {
+                                      final ppt = snapshot.data?['current']
+                                          ['precip_mm'];
+                                      return Text(
+                                        "  $ppt mm",
+                                        style: TextStyle(
+                                            fontFamily: 'SourceSans3',
+                                            color: Colors.white,
+                                            fontSize: 15),
+                                      );
+                                    }
+                                  },
+                                ),
+                                Icon(
+                                  WeatherIcons.sprinkle,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
                                 Text(
                                   'Precipitation',
                                   style: TextStyle(
                                       fontFamily: 'SourceSans3',
                                       color: Colors.white,
-                                      fontSize: 20),
+                                      fontSize: 15),
                                 )
                               ],
                             )
@@ -160,7 +269,7 @@ Future<Map<String, dynamic>> fetchWeatherData() async {
   final latitude = _locationData?.latitude;
   final longitude = _locationData?.longitude;
   final apiUrl =
-      "https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$latitude,$longitude";
+      "https://api.weatherapi.com/v1/current.json?key=$apiKey&q=28.7197355,77.0663015";
 
   final response = await http.get(Uri.parse(apiUrl));
   debugPrint('$latitude,$longitude');
